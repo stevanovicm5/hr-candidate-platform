86b1e08a6f09178ee9d5b300b7d4d24c7d3cb242
stevanovicm5 <stevanovicm1008@gmail.com>
2026-04-02 17:16:54 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/com/intens/hr_platform/HrPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intens.hr_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
